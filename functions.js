1ea91c21cf5b3fd5f661e1c70e8717b76a99560f
Ali Kore <=alikore4828@gmail.com>
2023-10-19 18:02:41 +0100

    infinite loop and arrow function

--- functions.js
+++ functions.js
@@ -1,4 +1,16 @@
 // write a simple JavaScript function called multipy that takes two numbers as params and returns the result to the caller
+function multiply (num1, num2) {
+    let result = num1 * num2
+    return result
+}
+
+let sum1 = multiply(20,30)
+console.log(sum1)
+
+let sum2 = multiply(20,20)
+console.log(sum2)
+
+
 // function multiply(num1, num2) {
 //     return num1 * num2
 // }
@@ -8,6 +20,15 @@
 
 
 // convert the function above to a variable
+
+let multiply = function (num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+let sum1 = multiply )(20,5)
+console.log(sum1)
+
 
 // let multiply =  function (num1, num2) {
 //     return num1 * num2
